fde82b410a4df74e6c97221e8c51dc827c157f35
chandankkrr <chandankkrr@gmail.com>
2021-02-09 17:50:03 +0700

    Initial commit

---
+++ Data/Location.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace threemillion.Data
+{
+    public class Location
+    {
+        public int Latitude { get; set; }
+
+        public int Longitude { get; set; }
+    }
+}
---
+++ Data/LocationDataService.cs
@@ -0,0 +1,35 @@
+﻿//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using Grpc.Core;
+//using ReactClient;
+
+//namespace threemillion.Data
+//{
+//    public class LocationDataService
+//    {
+//        private readonly LocationData.LocationDataClient _client;
+
+//        public LocationDataService(LocationData.LocationDataClient client)
+//        {
+//            _client = client;
+//        }
+
+//        public async Task<IEnumerable<Location>> GetAllLocationData()
+//        {
+//            var locations = new List<Location>();
+
+//            using var call = _client.GetAllLocationData(new GetLocationRequest { DataLimit = 10000 });
+
+//            await foreach (var response in call.ResponseStream.ReadAllAsync())
+//            {
+//                locations.Add(new Location
+//                {
+//                    Latitude = response.LatitudeE7,
+//                    Longitude = response.LongitudeE7
+//                });
+//            }
+
+//            return locations;
+//        }
+//    }
+//}
